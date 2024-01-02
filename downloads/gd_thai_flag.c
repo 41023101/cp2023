0151e6a77db5f9e8a8a88f5a3f64fa390d26dcaf
41023101 <113664308+41023101@users.noreply.github.com>
2024-01-02 14:42:57 +0800

    Add files via upload

--- downloads/gd_thai_flag.c
+++ downloads/gd_thai_flag.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <gd.h>
 #include <math.h>
-
+ 
 #define WIDTH 600
 #define HEIGHT 400
-
+ 
 void drawThaiFlag(gdImagePtr im) {
     int white = gdImageColorAllocate(im, 255, 255, 255);
     int indigo = gdImageColorAllocate(im, 46, 49, 146);  // 較深的靛藍色
     int red = gdImageColorAllocate(im, 255, 0, 0);
-
+ 
     // Draw white background
     gdImageFilledRectangle(im, 0, 0, WIDTH, HEIGHT, white);
-
+ 
     // Calculate stripe heights based on the specified ratio
     int totalStripes = 5;
     int stripeHeights[5] = {1, 1, 2, 1, 1};
@@ -20,7 +20,7 @@ void drawThaiFlag(gdImagePtr im) {
     for (int i = 0; i < totalStripes; ++i) {
         totalHeight += stripeHeights[i];
     }
-
+ 
     // Draw stripes
     int startY = 0;
     for (int i = 0; i < totalStripes; ++i) {
@@ -45,28 +45,28 @@ void drawThaiFlag(gdImagePtr im) {
         startY += stripeHeight;
     }
 }
-
+ 
 int main() {
     gdImagePtr im;
     FILE *outputFile;
-
+ 
     // Create a new image with specified width and height
     im = gdImageCreate(WIDTH, HEIGHT);
-
+ 
     // Draw the Thai flag on the image
     drawThaiFlag(im);
-
+ 
     // Open a file for writing
-    outputFile = fopen("./../images/thai_flag.png", "wb");
-
+    outputFile = fopen("thai_flag.png", "wb");
+ 
     // Write the image to the file in PNG format
     gdImagePng(im, outputFile);
-
+ 
     // Close the file
     fclose(outputFile);
-
+ 
     // Free the memory used for the image
     gdImageDestroy(im);
-
+ 
     return 0;
 }
